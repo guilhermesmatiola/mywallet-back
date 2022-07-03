0800d0cbf9f5e814fd1cf50a985f0cc7f7559684
guilhermesmatiola <guimatiola@gmail.com>
2022-07-03 04:49:40 -0300

    subindo modificacao2

--- src/dbStrategy/mongo.js
+++ src/dbStrategy/mongo.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const mongoClient = new MongoClient(process.env.MONGO_URL);
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 mongoClient.connect(() => {
   db = mongoClient.db(process.env.MONGO_DATABASE);
